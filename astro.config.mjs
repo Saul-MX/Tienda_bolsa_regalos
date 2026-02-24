2b3d557f0885162c6c114655dba7da209715dd54
Saul Torres <saul.domingueztorres.21@gmail.com>
2026-02-24 13:07:57 -0600

    Problema de commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://Saul-MX.github.io',
+  base: '/Tienda_bolsa_regalos',
   integrations: [mdx(), sitemap()],
 
   vite: {
